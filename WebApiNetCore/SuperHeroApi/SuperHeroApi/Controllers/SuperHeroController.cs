1553f38ac4b4cf0f82d802c25273f244db84a2e6
birukabel <biruk.abel@yahoo.com>
2023-01-15 16:46:11 -0800

    crud angular and net core api

---
+++ WebApiNetCore/SuperHeroApi/SuperHeroApi/Controllers/SuperHeroController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroApi.Data;
+
+namespace SuperHeroApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHero()
+        {
+            return Ok(await _context.SuperHeros.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> CreateSuperHero(SuperHero hero)
+        {
+            _context.SuperHeros.Add(hero);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.SuperHeros.ToArrayAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateSuperHero(SuperHero hero)
+        {
+            var dbhero = await _context.SuperHeros.FindAsync(hero.Id);
+            if (dbhero == null)
+                return BadRequest("Hero Not Found");
+            dbhero.Name = hero.Name;
+            dbhero.FirstName = hero.FirstName;
+            dbhero.LastName = hero.LastName;
+            dbhero.Place = hero.Place;
+
+            await _context.SaveChangesAsync();
+            return Ok(await _context.SuperHeros.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteSuperHero(int id)
+        {
+            var dbhero = await _context.SuperHeros.FindAsync(id);
+            if (dbhero == null)
+                return BadRequest("Hero Not Found");
+
+            _context.SuperHeros.Remove(dbhero);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.SuperHeros.ToListAsync());
+        }
+    }
+}
---
+++ WebApiNetCore/SuperHeroApi/SuperHeroApi/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroApi.Data
+{
+    public class DataContext:DbContext
+    {
+      
+        public DataContext(DbContextOptions<DataContext> options):base(options) { }
+
+        public DbSet<SuperHero> SuperHeros => Set<SuperHero>();
+    }
+}
